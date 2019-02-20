fd0181624a11381b1ebf801551c39fab5f02ace2
Vincent Galloro <vincentpetergalloro@gmail.com>
2019-02-20 01:15:08 -0500

    Reworked the load from file method to work with the new element builders

--- src/core/main/ui/ElementManager.java
+++ src/core/main/ui/ElementManager.java
@@ -3,15 +3,11 @@ package core.main.ui;
 import core.main.Mouse;
 import core.main.VGraphics;
 import core.main.structs.Vector;
-import core.main.ui.elements.impl.ColumnElement;
-import core.main.ui.elements.ElementFactory;
+import core.main.ui.elements.ElementBuilder;
+import core.main.ui.elements.ElementBuilderFactory;
 import core.main.ui.elements.IContainer;
 import core.main.ui.elements.IElement;
 import core.main.ui.elements.IListContainer;
-import core.main.ui.elements.impl.PaddingElement;
-import core.main.ui.elements.impl.RowElement;
-import core.main.ui.elements.impl.ScaleElement;
-import core.main.ui.elements.impl.TextElement;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -34,34 +30,20 @@ public class ElementManager {
                     int indent = 0;
                     while(line.charAt(indent)==' '){ indent++; }
                     String[] tokens = line.substring(indent).split(" ");
-                    IElement current = ElementFactory.fromString(tokens[0]);
-                    if(tokens.length >= 2){
-                        em.namedElements.put(tokens[1], current);
-                    }
-                    if(tokens.length >= 3){
-                        if(current instanceof TextElement){ 
-                            String[] words = new String[tokens.length-2];
-                            for(int i = 0; i < words.length; i++){
-                                words[i] = tokens[i+2];
-                            }
-                            ((TextElement)current).setText(String.join(" ", words)); 
+                    ElementBuilder currentBuilder = ElementBuilderFactory.fromString(tokens[0]);
+                    String elemName = null;
+                    for(int i = 1; i < tokens.length; i++){
+                        String[] argTokens = tokens[i].split("=");
+                        if(argTokens[0].equals("name")){
+                            elemName = argTokens[1];
                         }
-                        if(current instanceof PaddingElement){ 
-                            ((PaddingElement)current).setPadding(Integer.parseInt(tokens[2])); 
-                        }
-                        if(current instanceof ColumnElement){ 
-                            ((ColumnElement)current).setSeperation(Integer.parseInt(tokens[2])); 
-                        }
-                        if(current instanceof RowElement){ 
-                            ((RowElement)current).setSeperation(Integer.parseInt(tokens[2])); 
+                        else{
+                            currentBuilder.handleString(argTokens[0], argTokens[1]);
                         }
                     }
-                    if(tokens.length >= 4){
-                        if(current instanceof ScaleElement){ 
-                            ((ScaleElement)current).setSize(
-                                    new Vector(Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]))); 
-                        }
-                    }
+                    IElement current = currentBuilder.create();
+                    em.namedElements.put(elemName, current);
+                    
                     if(indent > 0){
                         if(chain.get(indent-1) instanceof IContainer){
                             ((IContainer)chain.get(indent-1)).setElement(current);
