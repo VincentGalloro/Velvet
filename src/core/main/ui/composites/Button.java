8fd687deee1cc62358144cb9b70f0a9cfffa8516
Vincent-PC\Vincent <VincentPeterGalloro@gmail.com>
2019-02-24 02:05:04 -0500

    Created the composites package along with the first concrete composite UI Element (Button)

---
+++ src/core/main/ui/composites/Button.java
@@ -0,0 +1,81 @@
+package core.main.ui.composites;
+
+import core.main.VGraphics;
+import core.main.structs.Vector;
+import core.main.ui.active.impl.TextColorTransition;
+import core.main.ui.elements.BasicElement;
+import core.main.ui.elements.ElementBuilder;
+import core.main.ui.elements.IBoxable;
+import core.main.ui.elements.IContainer;
+import core.main.ui.elements.IPaddable;
+import core.main.ui.elements.ISizeable;
+import core.main.ui.elements.ITextable;
+import core.main.ui.elements.impl.BoxElement;
+import core.main.ui.elements.impl.CenteredElement;
+import core.main.ui.elements.impl.PaddingElement;
+import core.main.ui.elements.impl.TextElement;
+import java.awt.Color;
+
+public class Button extends BasicElement implements IBoxable, ITextable, IPaddable, ISizeable{
+
+    public static class Builder extends BasicElement.Builder{
+
+        private Button button;
+        private ElementBuilder boxBuilder, textBuilder, padBuilder, sizeBuilder;
+        
+        public Builder() {
+            super(new Button());
+            button = (Button)get();
+            
+            boxBuilder = new BoxElement.Builder();
+            textBuilder = new TextElement.Builder();
+            padBuilder = new PaddingElement.Builder();
+            sizeBuilder = new CenteredElement.Builder();
+            
+            button.box = (IBoxable)boxBuilder.get();
+            button.text = (ITextable)textBuilder.get();
+            button.padding = (IPaddable)padBuilder.get();
+            button.sizing = (ISizeable)sizeBuilder.get();
+            
+            ((IContainer)button.box).setElement(button.sizing);
+            ((IContainer)button.sizing).setElement(button.padding);
+            ((IContainer)button.padding).setElement(button.text);
+            
+            button.setPadding(5);
+            button.setSize(new Vector(200, 40));
+            
+            button.addHoverHandler(new TextColorTransition(button.text, Color.BLACK, new Color(150, 150, 150)));
+        }
+        
+        public void handleString(String field, String value) {
+            super.handleString(field, value);
+            boxBuilder.handleString(field, value);
+            textBuilder.handleString(field, value);
+            padBuilder.handleString(field, value);
+            sizeBuilder.handleString(field, value);
+        }
+    }
+    
+    private IBoxable box;
+    private ITextable text;
+    private IPaddable padding;
+    private ISizeable sizing;
+    
+    private Button(){}
+    
+    public void setOutlineColor(Color o) { box.setOutlineColor(o); }
+    public void setFillColor(Color f) { box.setFillColor(f); }
+    public void setThickness(float t) { box.setThickness(t); }
+    public void setText(String t) { text.setText(t); }
+    public void setTextColor(Color c) { text.setTextColor(c); }
+    public void setPadding(double p) { padding.setPadding(p); }
+    public void setSize(Vector s) { sizing.setSize(s); }
+    
+    public Vector getSize() { return box.getSize(); }
+    public String getText() { return text.getText(); }
+    public Color getTextColor() { return text.getTextColor(); }
+    
+    public void render(VGraphics g) {
+        box.render(g);
+    }
+}
--- src/core/main/ui/elements/ElementBuilderFactory.java
+++ src/core/main/ui/elements/ElementBuilderFactory.java
@@ -1,6 +1,8 @@
 package core.main.ui.elements;
 
+import core.main.ui.composites.Button;
 import core.main.ui.elements.impl.BoxElement;
+import core.main.ui.elements.impl.CenteredElement;
 import core.main.ui.elements.impl.ColumnElement;
 import core.main.ui.elements.impl.ImageElement;
 import core.main.ui.elements.impl.PaddingElement;
@@ -11,13 +13,19 @@ import core.main.ui.elements.impl.TextElement;
 public class ElementBuilderFactory {
 
     public static ElementBuilder fromString(String s){
+        //base elements
         if(s.equals("box")){ return new BoxElement.Builder(); }
         if(s.equals("column")){ return new ColumnElement.Builder(); }
         if(s.equals("image")){ return new ImageElement.Builder(); }
         if(s.equals("padding")){ return new PaddingElement.Builder(); }
         if(s.equals("row")){ return new RowElement.Builder(); }
         if(s.equals("scale")){ return new ScaleElement.Builder(); }
+        if(s.equals("centered")){ return new CenteredElement.Builder(); }
         if(s.equals("text")){ return new TextElement.Builder(); }
+        
+        //composites
+        if(s.equals("button")){ return new Button.Builder(); }
+        
         return null;
     }
 }
