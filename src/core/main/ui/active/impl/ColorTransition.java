7da987d6f0a9d8fe07fa5c26c8189ef1cb3a88e3
Vincent-PC\Vincent <VincentPeterGalloro@gmail.com>
2019-02-27 02:29:35 -0500

    Added the Color Transitions which work using the new color adapters

---
+++ src/core/main/ui/active/impl/ColorTransition.java
@@ -0,0 +1,29 @@
+package core.main.ui.active.impl;
+
+import core.main.ui.active.IActivateable;
+import core.main.ui.active.IColorAdapter;
+import java.awt.Color;
+
+public class ColorTransition implements IActivateable{
+
+    private final IColorAdapter colorAdapter;
+    private final Color startColor, stopColor;
+    
+    public ColorTransition(IColorAdapter colorAdapter, Color startColor, Color stopColor){
+        this.colorAdapter = colorAdapter;
+        this.startColor = startColor;
+        this.stopColor = stopColor;
+    }
+    
+    public void onStart() {
+        if(startColor != null){
+            colorAdapter.setColor(startColor);
+        }
+    }
+
+    public void onStop() {
+        if(stopColor != null){
+            colorAdapter.setColor(stopColor);
+        }
+    }
+}
