caceca7fd1ac7683b5988528faab3377df658acb
Vincent-PC\Vincent <VincentPeterGalloro@gmail.com>
2019-03-14 00:44:21 -0400

    Extracted the text behaviour from Label into the new BasicTextable abstract class

---
+++ src/core/main/ui/elements/BasicTextable.java
@@ -0,0 +1,46 @@
+package core.main.ui.elements;
+
+import core.main.ui.active.adapters.impl.TextColorAdapter;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+
+public abstract class BasicTextable extends BasicElement implements ITextable{
+    
+    public static class Builder extends BasicElement.Builder{
+
+        private final BasicTextable textable;
+                
+        public Builder(BasicTextable textable) {
+            super(textable);
+            this.textable = (BasicTextable)get();
+        }
+        
+        public void handleString(String field, String value) {
+            super.handleString(field, value);
+            if(field.equals("text")){ textable.text = value; }
+            if(field.equals("text-color")){ textable.color = toColor(value); }
+        }
+    }
+        
+    protected static final Font font = new Font("Arial", Font.PLAIN, 24);
+    
+    protected String text;
+    protected FontMetrics fontMetrics;
+    protected Color color;
+    
+    public BasicTextable(){
+        Canvas c = new Canvas();
+        fontMetrics = c.getFontMetrics(font);
+        text = "";
+        color = Color.BLACK;
+    }
+    
+    public final void setText(String t){ text = t; }
+    public final void setTextColor(Color c){ color = c; }
+    
+    public final TextColorAdapter getTextColorAdapter(){ return new TextColorAdapter(this); }
+    public final String getText() { return text; } 
+    public final Color getTextColor() { return color; }
+}
--- src/core/main/ui/elements/impl/LabelElement.java
+++ src/core/main/ui/elements/impl/LabelElement.java
@@ -2,52 +2,15 @@ package core.main.ui.elements.impl;
 
 import core.main.VGraphics;
 import core.main.structs.Vector;
-import core.main.ui.active.adapters.impl.TextColorAdapter;
-import core.main.ui.elements.BasicElement;
-import core.main.ui.elements.ITextable;
-import java.awt.Canvas;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.FontMetrics;
+import core.main.ui.elements.BasicTextable;
 
-public class LabelElement extends BasicElement implements ITextable{
+public class LabelElement extends BasicTextable{
 
-    public static class Builder extends BasicElement.Builder{
-
-        private final LabelElement text;
-                
-        public Builder() {
-            super(new LabelElement());
-            text = (LabelElement)get();
-        }
-        
-        public void handleString(String field, String value) {
-            super.handleString(field, value);
-            if(field.equals("text")){ text.text = value; }
-            if(field.equals("color")){ text.color = toColor(value); }
-        }
-    }
-        
-    private static final Font font = new Font("Arial", Font.PLAIN, 24);
-    
-    private String text;
-    private FontMetrics fontMetrics;
-    private Color color;
-    
-    public LabelElement(){
-        Canvas c = new Canvas();
-        fontMetrics = c.getFontMetrics(font);
-        text = "";
-        color = Color.BLACK;
+    public static class Builder extends BasicTextable.Builder{
+        public Builder() { super(new LabelElement()); }
     }
     
-    public void setText(String t){ text = t; }
-    public void setTextColor(Color c){ color = c; }
-    
-    public TextColorAdapter getTextColorAdapter(){ return new TextColorAdapter(this); }
     public Vector getSize() { return new Vector(fontMetrics.stringWidth(text), fontMetrics.getAscent()+fontMetrics.getDescent()); }
-    public String getText() { return text; } 
-    public Color getTextColor() { return color; }
     
     public void render(VGraphics g) {
         g.setColor(color);
