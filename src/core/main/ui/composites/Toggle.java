229485f68eb1b3cb1489bb98e06dddaf8331c04d
Vincent-PC\Vincent <VincentPeterGalloro@gmail.com>
2019-03-29 02:33:59 -0400

    Reworked the Toggle to be built on top of the button element. TODO: Toggle will be able to swap out button color profiles when toggled on/off

--- src/core/main/ui/composites/Toggle.java
+++ src/core/main/ui/composites/Toggle.java
@@ -1,128 +1,73 @@
 package core.main.ui.composites;
 
-import core.main.VGraphics;
-import core.main.smooth.SmoothColor;
-import core.main.smooth.motion.Motion;
 import core.main.structs.Vector;
-import core.main.ui.active.impl.OffsetTransition;
 import core.main.ui.elements.BasicToggleable;
 import core.main.ui.elements.ElementBuilder;
 import core.main.ui.elements.IBoxable;
-import core.main.ui.elements.IContainer;
 import core.main.ui.elements.IPaddable;
 import core.main.ui.elements.ISizeable;
 import core.main.ui.elements.ITextable;
-import core.main.ui.elements.impl.BoxElement;
-import core.main.ui.elements.impl.CenteredElement;
-import core.main.ui.elements.impl.PaddingElement;
-import core.main.ui.elements.impl.LabelElement;
 import java.awt.Color;
 import java.awt.geom.AffineTransform;
-import java.util.function.Consumer;
 
 public class Toggle extends BasicToggleable implements IBoxable, ITextable, IPaddable, ISizeable{
+    
+    public static class ColorProfile{
+        
+        private Button.ColorProfile colorProfile, toggleColorProfile;
+        
+        //public ColorProfile
+    }
     
     public static class Builder extends BasicToggleable.Builder{
 
         private final Toggle toggle;
-        private final ElementBuilder boxBuilder, textBuilder, padBuilder, sizeBuilder;
-        //private final ColorData outlineData, fillData, textData;
+        private final ElementBuilder buttonBuilder;
         
         public Builder() {
             super(new Toggle());
             toggle = (Toggle)get();
             
-            //colorData = new ColorData();
+            buttonBuilder = new Button.Builder();
             
-            boxBuilder = new BoxElement.Builder();
-            textBuilder = new LabelElement.Builder();
-            padBuilder = new PaddingElement.Builder();
-            sizeBuilder = new CenteredElement.Builder();
+            toggle.button = (Button)buttonBuilder.get();
             
-            toggle.box = (IBoxable)boxBuilder.get();
-            toggle.text = (ITextable)textBuilder.get();
-            toggle.padding = (IPaddable)padBuilder.get();
-            toggle.sizing = (ISizeable)sizeBuilder.get();
+            toggle.addPostRenderHandler(toggle.button::render);
+            toggle.addUpdateHandler(toggle.button::update);
+            toggle.addHoverStartHandler(toggle.button::onHoverStart);
+            toggle.addHoverEndHandler(toggle.button::onHoverEnd);
+            toggle.addMousePressHandler(toggle.button::onMousePress);
             
-            ((IContainer)toggle.box).setElement(toggle.sizing);
-            ((IContainer)toggle.sizing).setElement(toggle.padding);
-            ((IContainer)toggle.padding).setElement(toggle.text);
-            
-            toggle.setPadding(5);
-            toggle.setSize(new Vector(200, 40));
-            
-            SmoothColor smoothTextColor = new SmoothColor(Color.BLACK);
-            smoothTextColor.setMotionFactory(Motion.linear(20));
-            SmoothColor smoothFillColor = new SmoothColor(Color.WHITE);
-            smoothFillColor.setMotionFactory(Motion.linear(20));
-            
-            toggle.addUpdateHandler(at -> smoothTextColor.update());
-            toggle.addUpdateHandler(at -> toggle.setTextColor(smoothTextColor.getSmooth()));
-            
-            toggle.addUpdateHandler(at -> smoothFillColor.update());
-            toggle.addUpdateHandler(at -> toggle.setFillColor(smoothFillColor.getSmooth()));
-            
-            toggle.addMousePressHandler(() -> toggle.toggle());
-            
-            Consumer<Color> multiText = c -> {
-                toggle.setOutlineColor(c);
-                smoothTextColor.overrideColor(c);
-            };
-            
-            /*toggle.addToggleOnHandler(() -> multiText.accept(colorData.getColor(false, true, false)));
-            toggle.addToggleOffHandler(() -> multiText.accept(colorData.getColor(false, false, false)));
-            
-            toggle.addToggleOnHandler(() -> smoothFillColor.overrideColor(colorData.getColor(false, true, true)));
-            toggle.addToggleOffHandler(() -> smoothFillColor.overrideColor(colorData.getColor(false, false, true)));
-            
-            toggle.addHoverStartHandler(() -> smoothTextColor.setColor(colorData.getColor(true, toggle.isToggled(), false)));
-            toggle.addHoverEndHandler(() -> smoothTextColor.setColor(colorData.getColor(false, toggle.isToggled(), false)));
-            
-            toggle.addHoverStartHandler(() -> smoothFillColor.setColor(colorData.getColor(true, toggle.isToggled(), true)));
-            toggle.addHoverEndHandler(() -> smoothFillColor.setColor(colorData.getColor(false, toggle.isToggled(), true)));*/
-            
-            OffsetTransition ot = new OffsetTransition();
-            toggle.addUpdateHandler(ot);
-            toggle.addRenderHandler(ot);
-            toggle.addHoverStartHandler(() -> ot.setOffset(new Vector(-1, -5)));
-            toggle.addHoverEndHandler(() -> ot.setOffset(new Vector()));
+            toggle.addMousePressHandler(toggle::toggle);
         }
         
         public void handleString(String field, String value) {
             super.handleString(field, value);
-            boxBuilder.handleString(field, value);
-            textBuilder.handleString(field, value);
-            padBuilder.handleString(field, value);
-            sizeBuilder.handleString(field, value);
+            buttonBuilder.handleString(field, value);
+            if(field.equals("text")){ toggle.addToggleOffHandler(() -> toggle.setText(value)); }
+            if(field.equals("toggle text")){ toggle.addToggleOnHandler(() -> toggle.setText(value)); }
         }
     }
     
-    private IBoxable box;
-    private ITextable text;
-    private IPaddable padding;
-    private ISizeable sizing;
+    private Button button;
     
     private Toggle(){}
     
-    public void setOutlineColor(Color o) { box.setOutlineColor(o); }
-    public void setFillColor(Color f) { box.setFillColor(f); }
-    public void setThickness(float t) { box.setThickness(t); }
-    public void setText(String t) { text.setText(t); }
-    public void setTextColor(Color c) { text.setTextColor(c); }
-    public void setPadding(double p) { padding.setPadding(p); }
-    public void setSize(Vector s) { sizing.setSize(s); }
+    public void setOutlineColor(Color o) { button.setOutlineColor(o); }
+    public void setFillColor(Color f) { button.setFillColor(f); }
+    public void setThickness(float t) { button.setThickness(t); }
+    public void setText(String t) { button.setText(t); }
+    public void setTextColor(Color c) { button.setTextColor(c); }
+    public void setPadding(double p) { button.setPadding(p); }
+    public void setSize(Vector s) { button.setSize(s); }
     
-    public void containerUpdate(AffineTransform at){ box.update(at); }
+    public void containerUpdate(AffineTransform at){ button.update(at); }
     
-    public Color getOutlineColor(){ return box.getOutlineColor(); }
-    public Color getFillColor(){ return box.getFillColor(); }
-    public Vector getSize() { return box.getSize(); }
-    public String getText() { return text.getText(); }
-    public Color getTextColor() { return text.getTextColor(); }
-    public boolean supportsNewline(){ return text.supportsNewline(); }
-    public AffineTransform getCharTransform(int charIndex) { return text.getCharTransform(charIndex); }
-    
-    public void onRender(VGraphics g) {
-        box.render(g);
-    }
+    public Color getOutlineColor(){ return button.getOutlineColor(); }
+    public Color getFillColor(){ return button.getFillColor(); }
+    public Vector getSize() { return button.getSize(); }
+    public String getText() { return button.getText(); }
+    public Color getTextColor() { return button.getTextColor(); }
+    public boolean supportsNewline(){ return button.supportsNewline(); }
+    public AffineTransform getCharTransform(int charIndex) { return button.getCharTransform(charIndex); }
 }
