f0410d8f082711f55eca1cfcdbee6837203242bf
Vincent-PC\Vincent <VincentPeterGalloro@gmail.com>
2019-03-29 02:33:16 -0400

    Extracted Color related behaviour into the ColorProfile and ColorController classes. Added full support for color transitions from VelvetScript

--- src/core/main/ui/composites/Button.java
+++ src/core/main/ui/composites/Button.java
@@ -4,22 +4,83 @@ import core.main.VGraphics;
 import core.main.smooth.SmoothColor;
 import core.main.smooth.motion.Motion;
 import core.main.structs.Vector;
+import core.main.ui.active.IUpdateable;
 import core.main.ui.active.impl.OffsetTransition;
-import core.main.ui.elements.BasicElement;
-import core.main.ui.elements.ElementBuilder;
-import core.main.ui.elements.IBoxable;
-import core.main.ui.elements.IContainer;
-import core.main.ui.elements.IPaddable;
-import core.main.ui.elements.ISizeable;
-import core.main.ui.elements.ITextable;
-import core.main.ui.elements.impl.BoxElement;
-import core.main.ui.elements.impl.CenteredElement;
-import core.main.ui.elements.impl.PaddingElement;
-import core.main.ui.elements.impl.LabelElement;
+import core.main.ui.elements.*;
+import core.main.ui.elements.impl.*;
 import java.awt.Color;
 import java.awt.geom.AffineTransform;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class Button extends BasicElement implements IBoxable, ITextable, IPaddable, ISizeable{
+    
+    private static class ColorController implements IUpdateable{
+        
+        private SmoothColor color;
+        
+        public ColorController(Color c){
+            color = new SmoothColor(c);
+            color.setMotionFactory(Motion.linear(15));
+        }
+        
+        public void bind(IElement source, Supplier<ColorProfile.CHCProfile> profile, Consumer<Color> target){
+            source.addHoverStartHandler(() -> { if(profile.get().hoverColor != null){ color.setColor(profile.get().hoverColor); } });
+            source.addHoverEndHandler(() -> { if(profile.get().color != null){ color.setColor(profile.get().color); } });
+            source.addMousePressHandler(() -> { if(profile.get().clickColor != null){ color.setSmooth(profile.get().clickColor); } });
+            source.addUpdateHandler(this);
+            source.addUpdateHandler(at -> target.accept(color.getSmooth()));
+        }
+
+        public void update(AffineTransform at) {
+            color.update();
+        }
+    }
+    
+    public static class ColorProfile{
+        
+        public static class CHCProfile{
+            private Color color, hoverColor, clickColor;
+
+            public Color getColor() { return color; } 
+            public void setColor(Color color) { this.color = color; } 
+
+            public Color getHoverColor() { return hoverColor; } 
+            public void setHoverColor(Color hoverColor) { this.hoverColor = hoverColor; } 
+
+            public Color getClickColor() { return clickColor; } 
+            public void setClickColor(Color clickColor) { this.clickColor = clickColor; }
+
+            public void handleString(String field, Color color){
+                if(field.equals("color")){ this.color = color; }
+                if(field.equals("hover color")){ hoverColor = color; }
+                if(field.equals("click color")){ clickColor = color; }
+            }
+        }
+        
+        private CHCProfile boxOutlineProfile, boxFillProfile, textProfile;
+        
+        public ColorProfile(){
+            boxOutlineProfile = new CHCProfile();
+            boxFillProfile = new CHCProfile();
+            textProfile = new CHCProfile();
+        }
+
+        public CHCProfile getBoxOutlineProfile() { return boxOutlineProfile; }
+        public void setBoxOutlineProfile(CHCProfile boxOutlineProfile) { this.boxOutlineProfile = boxOutlineProfile; }
+
+        public CHCProfile getBoxFillProfile() { return boxFillProfile; }
+        public void setBoxFillProfile(CHCProfile boxFillProfile) { this.boxFillProfile = boxFillProfile; }
+
+        public CHCProfile getTextProfile() { return textProfile; }
+        public void setTextProfile(CHCProfile textProfile) { this.textProfile = textProfile; }
+
+        public void handleString(String field, Color color){
+            if(field.startsWith("outline ")){ boxOutlineProfile.handleString(field.split(" ", 2)[1], color); }
+            if(field.startsWith("fill ")){ boxFillProfile.handleString(field.split(" ", 2)[1], color); }
+            if(field.startsWith("text ")){ textProfile.handleString(field.split(" ", 2)[1], color); }
+        }
+    }
     
     public static class Builder extends BasicElement.Builder{
 
@@ -40,6 +101,8 @@ public class Button extends BasicElement implements IBoxable, ITextable, IPaddab
             button.padding = (IPaddable)padBuilder.get();
             button.sizing = (ISizeable)sizeBuilder.get();
             
+            button.addPostRenderHandler(button.box::render);
+            
             ((IContainer)button.box).setElement(button.sizing);
             ((IContainer)button.sizing).setElement(button.padding);
             ((IContainer)button.padding).setElement(button.text);
@@ -47,18 +110,22 @@ public class Button extends BasicElement implements IBoxable, ITextable, IPaddab
             button.setPadding(5);
             button.setSize(new Vector(200, 40));
             
-            SmoothColor smoothColor = new SmoothColor(Color.BLACK);
-            smoothColor.setMotionFactory(Motion.linear(20));
+            ColorController boxOutlineCC = new ColorController(Color.BLACK);
+            ColorController boxFillCC = new ColorController(Color.WHITE);
+            ColorController textCC = new ColorController(Color.BLACK);
             
-            button.addUpdateHandler(at -> smoothColor.update());
-            button.addUpdateHandler(at -> button.setTextColor(smoothColor.getSmooth()));
-            button.addHoverStartHandler(() -> smoothColor.setColor(new Color(200, 200, 200)));
-            button.addHoverEndHandler(() -> smoothColor.setColor(Color.BLACK));
-            button.addMousePressHandler(() -> smoothColor.setSmooth(new Color(0, 255, 0)));
+            boxOutlineCC.bind(button, () -> button.colorProfile.boxOutlineProfile, c -> button.setOutlineColor(c));
+            boxFillCC.bind(button, () -> button.colorProfile.boxFillProfile, c -> button.setFillColor(c));
+            textCC.bind(button, () -> button.colorProfile.textProfile, c -> button.setTextColor(c));
+            
+            button.colorProfile = new ColorProfile();
+            
+            button.colorProfile.textProfile.color = Color.BLACK;
+            button.colorProfile.textProfile.hoverColor = new Color(200, 200, 200);
+            button.colorProfile.textProfile.clickColor = Color.GREEN;
             
             OffsetTransition ot = new OffsetTransition();
-            button.addUpdateHandler(ot);
-            button.addRenderHandler(ot);
+            ot.apply(button);
             button.addHoverStartHandler(() -> ot.setOffset(new Vector(-1, -5)));
             button.addHoverEndHandler(() -> ot.setOffset(new Vector()));
         }
@@ -69,6 +136,7 @@ public class Button extends BasicElement implements IBoxable, ITextable, IPaddab
             textBuilder.handleString(field, value);
             padBuilder.handleString(field, value);
             sizeBuilder.handleString(field, value);
+            if(field.endsWith("color")){ button.colorProfile.handleString(field, toColor(value)); }
         }
     }
     
@@ -76,6 +144,7 @@ public class Button extends BasicElement implements IBoxable, ITextable, IPaddab
     private ITextable text;
     private IPaddable padding;
     private ISizeable sizing;
+    private ColorProfile colorProfile;
     
     private Button(){}
     
@@ -86,6 +155,7 @@ public class Button extends BasicElement implements IBoxable, ITextable, IPaddab
     public void setTextColor(Color c) { text.setTextColor(c); }
     public void setPadding(double p) { padding.setPadding(p); }
     public void setSize(Vector s) { sizing.setSize(s); }
+    public void setColorProfile(ColorProfile colorProfile) { this.colorProfile = colorProfile; }
     
     public void containerUpdate(AffineTransform at){ box.update(at); }
     
@@ -96,8 +166,5 @@ public class Button extends BasicElement implements IBoxable, ITextable, IPaddab
     public Color getTextColor() { return text.getTextColor(); }
     public boolean supportsNewline(){ return text.supportsNewline(); }
     public AffineTransform getCharTransform(int charIndex) { return text.getCharTransform(charIndex); }
-    
-    public void onRender(VGraphics g) {
-        box.render(g);
-    }
+    public ColorProfile getColorProfile() { return colorProfile; }
 }
