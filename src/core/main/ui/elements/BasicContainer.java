d471687c5d9f62033bdb9ed53dc95117b67b062b
Vincent Galloro <vincentpetergalloro@gmail.com>
2019-03-30 02:44:30 -0400

    Added Pre/Post-ChildRenderHandlers for IContainers, which function similair to pre/post-renderHandlers, but only affect the child object and not the parts

--- src/core/main/ui/elements/BasicContainer.java
+++ src/core/main/ui/elements/BasicContainer.java
@@ -3,7 +3,9 @@ package core.main.ui.elements;
 
 import core.main.VGraphics;
 import core.main.structs.Vector;
+import core.main.ui.active.IRenderable;
 import java.awt.geom.AffineTransform;
+import java.util.ArrayList;
 
 public abstract class BasicContainer extends BasicElement implements IContainer{
 
@@ -14,9 +16,6 @@ public abstract class BasicContainer extends BasicElement implements IContainer{
         public Builder(BasicContainer container) {
             super(container);
             this.container = container;
-            
-            container.addUpdateHandler(container::childUpdate);
-            container.addPostRenderHandler(container::childRender);
         }
         
         public void handleString(String field, String value) {
@@ -25,7 +24,19 @@ public abstract class BasicContainer extends BasicElement implements IContainer{
         }
     }
     
+    private final ArrayList<IRenderable> preChildRenderHandlers, postChildRenderHandlers;
     protected IElement element;
+    
+    public BasicContainer(){
+        this.preChildRenderHandlers = new ArrayList<>();
+        this.postChildRenderHandlers = new ArrayList<>();
+        
+        addUpdateHandler(this::childUpdate);
+        addPostRenderHandler(this::childRender);
+    }
+    
+    public void addPreChildRenderHandler(IRenderable rendereable){ preChildRenderHandlers.add(rendereable); }
+    public void addPostChildRenderHandler(IRenderable rendereable){ postChildRenderHandlers.add(rendereable); }
     
     public final void setElement(IElement e) { element = e; }
     
@@ -51,7 +62,13 @@ public abstract class BasicContainer extends BasicElement implements IContainer{
         if(element != null){
             g.save();
             g.transform(getTransform());
+            for(int i = preChildRenderHandlers.size()-1; i>=0; i--){ 
+                preChildRenderHandlers.get(i).render(g); 
+            }
             element.render(g);
+            for(IRenderable r : postChildRenderHandlers){ 
+                r.render(g); 
+            }
             g.reset();
         }
     }
--- src/core/main/ui/elements/IContainer.java
+++ src/core/main/ui/elements/IContainer.java
@@ -1,9 +1,13 @@
 
 package core.main.ui.elements;
 
+import core.main.ui.active.IRenderable;
 import java.awt.geom.AffineTransform;
 
 public interface IContainer extends IElement{
+    
+    public void addPreChildRenderHandler(IRenderable rendereable);
+    public void addPostChildRenderHandler(IRenderable rendereable);
     
     public void setElement(IElement e);
     
