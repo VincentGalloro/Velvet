b13eea3a27db33dc608596b74909251c9f069a79
Vincent Galloro <vincentpetergalloro@gmail.com>
2019-03-31 17:33:44 -0400

    Added directional padding support to IPaddables and the PaddingElement

--- src/core/main/ui/elements/IPaddable.java
+++ src/core/main/ui/elements/IPaddable.java
@@ -1,6 +1,9 @@
 package core.main.ui.elements;
 
+import core.main.structs.GridDirection;
+
 public interface IPaddable extends IElement{
 
     public void setPadding(double p);
+    public void setPadding(double p, GridDirection dir);
 }
--- src/core/main/ui/elements/impl/PaddingElement.java
+++ src/core/main/ui/elements/impl/PaddingElement.java
@@ -1,5 +1,6 @@
 package core.main.ui.elements.impl;
 
+import core.main.structs.GridDirection;
 import core.main.structs.Vector;
 import core.main.ui.elements.BasicContainer;
 import core.main.ui.elements.IPaddable;
@@ -9,6 +10,8 @@ public class PaddingElement extends BasicContainer implements IPaddable{
 
     public static class Builder extends BasicContainer.Builder{
 
+        private static final String[] DIRS = {"top", "right", "bottom", "left"};
+        
         private final PaddingElement padding;
                 
         public Builder() {
@@ -18,22 +21,47 @@ public class PaddingElement extends BasicContainer implements IPaddable{
         
         public void handleString(String field, String value) {
             super.handleString(field, value);
-            if(field.equals("padding")){ padding.padding = Float.parseFloat(value); }
+            if(field.equals("padding")){ padding.setPadding(Float.parseFloat(value)); }
+            if(field.endsWith(" padding")){
+                String[] tokens = field.split(" ");
+                for(int i = 0; i < DIRS.length; i++){
+                    if(tokens[0].equals(DIRS[i])){
+                        padding.setPadding(Float.parseFloat(value), GridDirection.values()[i]);
+                        break;
+                    }
+                }
+            }
         }
     } 
     
-    private double padding;
+    private final double[] padding;
     
-    public void setPadding(double p){ padding = p; }
+    public PaddingElement(){
+        this.padding = new double[4];
+    }
+    
+    public PaddingElement(double[] padding){
+        this.padding = padding;
+    }
+    
+    public void setPadding(double p){
+        for(int i = 0; i < 4; i++){ padding[i] = p; }
+    }
+    public void setPadding(double p, GridDirection dir){
+        padding[dir.ordinal()] = p;
+    }
     
     public Vector getSize() {
-        if(element == null){ return new Vector(); }
-        return element.getSize().add(padding*2);
+        Vector size = new Vector(padding[GridDirection.LEFT.ordinal()]+padding[GridDirection.RIGHT.ordinal()], 
+                padding[GridDirection.UP.ordinal()]+padding[GridDirection.DOWN.ordinal()]);
+        
+        if(element == null){ return size; }
+        return element.getSize().add(size);
     }
     
     public AffineTransform getTransform(){
         AffineTransform at = new AffineTransform();
-        at.translate(padding, padding);
+        at.translate(padding[3], padding[0]);
         return at;
     }
 }
