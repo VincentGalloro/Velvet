8390680089e94b3ba2b0c9d60b36709e6b4c5992
Vincent-PC\Vincent <VincentPeterGalloro@gmail.com>
2019-04-09 23:06:15 -0400

    Reorganized many of the VelvetDocumentation UI files. Added more customElement support

--- VelvetDocumentation/src/CustomElements.java
+++ VelvetDocumentation/src/CustomElements.java
@@ -1,19 +1,53 @@
 
 import core.main.Mouse;
+import core.main.ui.UIController;
+import core.main.ui.UIHandler;
+import core.main.ui.composites.BasicScrollBar;
 import core.main.ui.elements.ElementFactory;
 import core.main.ui.elements.IElement;
 import core.main.ui.elements.IElementFactory;
+import core.main.ui.elements.impl.BoxElement;
+import java.awt.Color;
 
 public class CustomElements implements IElementFactory{
     
+    public static class ColorBoxFactory{
+        
+        private static String[] BAR_NAMES = {"red bar", "green bar", "blue bar", "rounding bar"};
+        
+        public static IElement create(UIHandler uiHandler){
+            UIController controller = uiHandler.loadController("ui/custom_elems/color_box.txt");
+            
+            BoxElement box = (BoxElement)controller.getElement("box");
+            
+            BasicScrollBar[] bars = new BasicScrollBar[BAR_NAMES.length];
+            for(int i = 0; i < BAR_NAMES.length; i++){ bars[i] = (BasicScrollBar)controller.getElement(BAR_NAMES[i]); }
+            
+            float[] c = new float[3];
+            for(int i = 0; i < 3; i++){
+                final int index = i;
+                bars[i].addDeltaHandler(d -> {
+                    c[index] = (float)d;
+                    box.setFillColor(new Color(c[0],c[1],c[2]));
+                });
+            }
+            bars[3].addDeltaHandler(d -> box.setRounding(d*60));
+                             
+            return controller.getRoot();
+        }
+    }
+    
+    private final UIHandler uiHandler;
     private ElementFactory ebf;
     
-    public CustomElements(){
+    public CustomElements(UIHandler uiHandler){
+        this.uiHandler = uiHandler;
         ebf = new ElementFactory();
     }
     
     public IElement fromString(String name, Mouse mouse){
         if(name.equals("pulse line")){ return null; }
+        if(name.equals("color box")){ return ColorBoxFactory.create(uiHandler); }
         
         return ebf.fromString(name, mouse);
     }
--- VelvetDocumentation/src/Level.java
+++ VelvetDocumentation/src/Level.java
@@ -18,14 +18,16 @@ public class Level extends Velvet{
     public Level() { super(new Point(1300, 700)); }
 
     public void init() {
-        customElements = new CustomElements();
         uiHandler = new UIHandler(mouse, keyboard);
+        customElements = new CustomElements(uiHandler);
         uiHandler.setElementBuilderFactory(customElements);
         
         UIController main = uiHandler.loadController("ui/main.txt");
         uiHandler.setRoot(main);
         primaryPanel = new SwapPanel((IContainer)main.getElement("primary panel"));
         secondaryPanel = new SwapPanel((IContainer)main.getElement("secondary panel"));
+        
+        main.getElement("logo").addMousePressHandler(() -> { init(); });
         
         main.getNamedElements().filter(e -> e instanceof Button).forEach(b -> {
             UIController panel = uiHandler.loadController("ui/"+b.getName()+"/panel.txt");
@@ -39,6 +41,15 @@ public class Level extends Velvet{
                         innerB.addMousePressHandler(() -> {
                             secondaryPanel.load((IListContainer)layout.getRoot());
                         });
+                    }
+                    
+                    //temp debug code
+                    //automatically open a certain panel by simulating button clicks
+                    if(innerB.getName().equals("ui_package")){
+                        innerB.onMousePress();
+                        innerB.onMouseRelease();
+                        b.onMousePress();
+                        b.onMouseRelease();
                     }
                 });
             }
