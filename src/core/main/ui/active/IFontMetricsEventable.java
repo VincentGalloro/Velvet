0f0b6bc15fc1b6bae0471b4732755a642be340cc
Vincent Galloro <vincentpetergalloro@gmail.com>
2019-04-15 15:55:14 -0400

    Added new active interfaces

---
+++ src/core/main/ui/active/IFontMetricsEventable.java
@@ -0,0 +1,9 @@
+
+package core.main.ui.active;
+
+import java.awt.FontMetrics;
+
+public interface IFontMetricsEventable {
+    
+    public void onFontMetrics(FontMetrics fm);
+}
---
+++ src/core/main/ui/active/ITextEventable.java
@@ -0,0 +1,7 @@
+
+package core.main.ui.active;
+
+public interface ITextEventable {
+    
+    public void onText(String text);
+}
