f772545290c39b5529aad71d8a1d19b51a5c805a
Vincent <vincentpetergalloro@gmail.com>
2020-11-21 19:51:16 -0500

    Added the camera controller for user controlled camera movements

--- src/velvet/game/CameraController.kt
+++ src/velvet/game/CameraController.kt
@@ -1,7 +1,33 @@
 package velvet.game
 
-import velvet.ui.components.UIComponent
+import velvet.ui.components.BasicComponent
+import velvet.util.types.spatial.Vector
+import java.awt.event.MouseEvent
+import kotlin.math.pow
 
-/*
-class CameraController : UIComponent {
-}*/
+class CameraController(private val camera: Camera) : BasicComponent() {
+
+    private var worldAnchor: Vector = Vector()
+
+    init{
+        uiEventListener.onMouseButtonPressed = { event, state ->
+            if(event.button == MouseEvent.BUTTON2) {
+                worldAnchor = camera.fromScreenPos(state.mousePos)
+            }
+        }
+        uiEventListener.onMouseMoved = { event, state ->
+            if(MouseEvent.BUTTON2 in state.mouseButtonsDown){
+                camera.targetBounds = (camera.targetBounds ?: camera.bounds)
+                        .setPos(worldAnchor, camera.windowBounds.getAnchor(event.pos))
+            }
+        }
+        uiEventListener.onMouseWheelScrolled = { event, state ->
+            if(MouseEvent.BUTTON2 !in state.mouseButtonsDown){
+                worldAnchor = camera.fromScreenPos(state.mousePos)
+            }
+            camera.targetBounds = (camera.targetBounds ?: camera.bounds)
+                    .scale(Vector(0.9.pow(-event.amount)), Vector())
+                    .setPos(worldAnchor, camera.windowBounds.getAnchor(state.mousePos))
+        }
+    }
+}
